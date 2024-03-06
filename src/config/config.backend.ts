8a97ef5f856b778999c9fb74a719cc0cf41ba6d6
Chakhrit-107 <Chakhrit.Phoowamukda@gmail.com>
2024-03-07 05:03:34 +0700

    update url back end

--- src/config/config.backend.ts
+++ src/config/config.backend.ts
@@ -1,7 +1,8 @@
-import {api_url, api_urlX} from "./env"
+export const api_url = "https://sign-language-server.onrender.com";
+export const api_urlX = "http://3.27.222.170:8080";
 
 export default {
-  API_URL: api_url,
+  API_URL: api_urlX,
   AUTHENTICATION: "authentication",
   LOGIN: "login",
   REGISTER: "register",
--- src/config/env.ts
+++
@@ -1,2 +0,0 @@
-export const api_url = "https://sign-language-server.onrender.com";
-export const api_urlX = "http://13.211.176.140:8080";
